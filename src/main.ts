3a6b7cbd0b17b0e8775bf9b7e47d84b2e58f017b
dromanenko <7878dima7878@gmail.com>
2023-08-31 02:20:42 +0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,14 @@ const start = async () => {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
       origin: ['https://www.78dima.com', 'https://www.api.78dima.com'],
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      allowedHeaders:
-        'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+      allowedHeaders: [
+        'X-Requested-With',
+        'X-HTTP-Method-Override',
+        'Content-Type',
+        'Accept',
+        'Observe',
+      ],
       credentials: true,
     });
     app.useGlobalPipes(new ValidationPipe());
