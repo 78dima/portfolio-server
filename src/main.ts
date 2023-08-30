ddd67d98380c8be21753e70c7a6efdf0504354fc
dromanenko <7878dima7878@gmail.com>
2023-08-31 01:39:24 +0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,23 @@ const start = async () => {
     app.enableCors({
       origin: ['https://www.78dima.com', 'https://www.api.78dima.com'],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders:
+        'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
       credentials: true,
     });
     app.useGlobalPipes(new ValidationPipe());
+    app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', 'https://www.78dima.com');
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,HEAD,PUT,PATCH,POST,DELETE',
+      );
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
+      );
+      next();
+    });
 
     await app.listen(PORT, () => console.log('Server starts on ', PORT));
   } catch (e) {
