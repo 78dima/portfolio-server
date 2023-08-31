c3acc125ae763bf1ad83a081893618c5e06d120e
dromanenko <7878dima7878@gmail.com>
2023-09-01 00:47:50 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ const start = async () => {
   try {
     const PORT = process.env.PORT || 8000;
     const app = await NestFactory.create(AppModule);
-    app.enableCors({
-      origin: '*',
-      credentials: true,
-    });
+    // app.enableCors({
+    //   origin: '*',
+    //   credentials: true,
+    // });
     app.useGlobalPipes(new ValidationPipe());
 
     await app.listen(PORT, () => console.log('Server starts on ', PORT));
