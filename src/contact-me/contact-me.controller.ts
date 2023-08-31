f100c6d0a9bd98e85b7344660219d3628d220bc2
dromanenko <7878dima7878@gmail.com>
2023-08-31 16:49:04 +0300

    update

--- src/contact-me/contact-me.controller.ts
+++ src/contact-me/contact-me.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ContactMeService } from './contact-me.service';
 import { Public } from 'src/common/decorators/public.decorator';
 import { CreateContactmeDto } from './contracts/create-contact-me.dto';
-import { AtGuard } from 'src/common/guards/at.guard';
 
 @Controller('api/contact-me')
 export class ContactMeController {
