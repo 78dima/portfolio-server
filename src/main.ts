02202f997fc5efbc12b74adfb403b36894fbaa77
dromanenko <7878dima7878@gmail.com>
2023-09-01 19:40:08 +0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,14 @@ const start = async () => {
   try {
     const PORT = process.env.PORT || 8000;
     const app = await NestFactory.create(AppModule);
-    // app.enableCors({
-    //   origin: '*',
-    //   credentials: true,
-    // });
+    app.enableCors({
+      origin: [
+        'https://78dima.com',
+        'https://www.78dima.com',
+        'http://localhost:3000',
+      ],
+      credentials: true,
+    });
     app.useGlobalPipes(new ValidationPipe());
 
     await app.listen(PORT, () => console.log('Server starts on ', PORT));
